77fe8e5366010292d3a07993802af51c5209db0d
NBrockveld <brockveld.nikolas@gmail.com>
2025-03-03 10:01:36 -0300

    DS 3

---
+++ nikolas_brockveld_cardoso_Ag3_DS_III/MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
@@ -0,0 +1,31 @@
+using MauiAppMinhasCompras.Models;
+
+namespace MauiAppMinhasCompras.Views;
+
+public partial class NovoProduto : ContentPage
+{
+	public NovoProduto()
+	{
+		InitializeComponent();
+	}
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			Produto p = new Produto
+			{
+				Descricao = txt_descricao.Text,
+				Quantidade = Convert.ToDouble(txt_quantidade.Text),
+				Preco = Convert.ToDouble(txt_preco.Text)
+			};
+
+			await App.Db.Insert(p);
+			await DisplayAlert("Sucesso!", "Registros Inseridos", "ok");
+
+		} catch(Exception ex)
+		{
+			await DisplayAlert("não", ex.Message, "ok");
+		}
+    }
+}
